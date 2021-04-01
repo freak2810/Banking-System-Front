ef3cd4e3ca9ffc16dd95228d430231d229531c2f
freak2810 <adityamanikanthrao@gmail.com>
2021-04-01 13:17:44 +0530

    added eslint + prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
+// @ts-ignore
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
