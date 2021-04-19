574d3b5ff91bb713d296014a258d7a4650024e80
freak2810 <adityamanikanthrao@gmail.com>
2021-04-19 18:33:41 +0530

    added explicit conversion

--- utils/security.ts
+++ utils/security.ts
@@ -7,20 +7,20 @@ export async function encryptValue(
 	value: number,
 	publicKey: PublicKey
 ): Promise<bigint> {
-	const newPublicKey = {
+	const newPublicKey: { n: bigint; g: bigint } = {
 		n: BigInt(publicKey.n),
 		g: BigInt(publicKey.g),
 	};
 
-	const { n, g } = newPublicKey;
-	const nSquared = n * n;
+	const { n, g }: { n: bigint; g: bigint } = newPublicKey;
+	const nSquared: bigint = (n * n) as bigint;
 
-	const r = randBetween(n * n, BigInt(1));
+	const r: bigint = randBetween(n * n, BigInt(1)) as bigint;
 
-	const c1 = modPow(g, BigInt(value), nSquared);
-	const c2 = modPow(r, n, nSquared);
+	const c1: bigint = modPow(g, BigInt(value), nSquared) as bigint;
+	const c2: bigint = modPow(r, n, nSquared) as bigint;
 
-	return (c1 * c2) % nSquared;
+	return ((c1 * c2) % nSquared) as bigint;
 }
 
 export async function decryptValue(
@@ -28,22 +28,22 @@ export async function decryptValue(
 	publicKey: PublicKey,
 	privateKey: PrivateKey
 ): Promise<bigint> {
-	const cipherText = BigInt(cipher);
+	const cipherText = BigInt(cipher) as bigint;
 
-	const newPublicKey = {
+	const newPublicKey: { n: bigint; g: bigint } = {
 		n: BigInt(publicKey.n),
 		g: BigInt(publicKey.g),
 	};
 
-	const newPrivateKey = {
-		lambda: BigInt(privateKey.lambda),
-		mu: BigInt(privateKey.mu),
+	const newPrivateKey: { lambda: bigint; mu: bigint } = {
+		lambda: BigInt(privateKey.lambda) as bigint,
+		mu: BigInt(privateKey.mu) as bigint,
 	};
 
-	const { n } = newPublicKey;
-	const { lambda, mu } = newPrivateKey;
+	const { n }: { n: bigint; g: bigint } = newPublicKey;
+	const { lambda, mu }: { lambda: bigint; mu: bigint } = newPrivateKey;
 
-	const alpha = modPow(cipherText, lambda, n * n);
+	const alpha: bigint = modPow(cipherText, lambda, n * n) as bigint;
 
-	return (L(alpha, n) * mu) % n;
+	return ((L(alpha, n) * mu) % n) as bigint;
 }
