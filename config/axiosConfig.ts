0209cde24518978c39da3d995af729ae621812d2
freak2810 <adityamanikanthrao@gmail.com>
2021-05-02 19:42:51 +0530

    changed axios base endpoint

--- config/axiosConfig.ts
+++ config/axiosConfig.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// export default axios.create({
-// 	baseURL: 'https://banking-he.herokuapp.com/api/',
-// });
-
 export default axios.create({
-	baseURL: 'http://localhost:8000/api/',
+	baseURL: 'https://banking-he.herokuapp.com/api/',
 });
+
+// export default axios.create({
+// 	baseURL: 'http://localhost:8000/api/',
+// });
